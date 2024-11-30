43290ccf3622f0127bfedac52e4805d51f267c01
Mario VB <mario.villasante@ciencias.unam.mx>
2024-11-30 22:32:38 +0000

    ♻️  Refactor

--- huevolizer/dummy.py
+++ huevolizer/dummy.py
@@ -1,7 +1,3 @@
 def set_eggs_by_hen(raw_data):
-    raw_data["huevos_by_hen"] = 1
+    raw_data["eggs_by_hen"] = 1
     return raw_data
-
-
-def add_offset(augend: int, addend: int) -> int:
-    return augend + addend
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -5,13 +5,5 @@ import pandas as pd
 def test_set_data():
     data = pd.DataFrame({"Huevos": [1, 2, 3], "Gallinas": [1, 2, 3]})
     obtained = dt.set_eggs_by_hen(data)
-    expected_column = "huevos_by_hen"
+    expected_column = "eggs_by_hen"
     assert expected_column in obtained.columns
-
-
-def test_add_offset():
-    augend = 1
-    addend = 2
-    expected = augend + addend
-    obtained = dt.add_offset(augend, addend)
-    assert expected == obtained
