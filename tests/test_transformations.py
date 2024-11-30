9a887086261cd1f7765114b0efad262f16080876
Mario VB <mario.villasante@ciencias.unam.mx>
2024-11-30 22:27:32 +0000

    🛑🧪 Fail tests

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,4 +1,12 @@
 import huevolizer as dt
+import pandas as pd
+
+
+def test_set_data():
+    data = pd.DataFrame({"Huevos": [1, 2, 3], "Gallinas": [1, 2, 3]})
+    obtained = dt.set_eggs_by_hen(data)
+    expected_column = "huevos_by_hen"
+    assert expected_column in obtained.columns
 
 
 def test_add_offset():
