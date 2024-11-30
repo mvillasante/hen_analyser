2f98ef0a795591cfb00bc37c4b89594612f6efa7
Mario VB <mario.villasante@ciencias.unam.mx>
2024-11-30 22:44:29 +0000

    ♻️  Refactor

--- tests/test_control_limits.py
+++ tests/test_control_limits.py
@@ -1,9 +1,9 @@
-import huevolizer as dt
+from huevolizer.control_limits import set_eggs_by_hen
 import pandas as pd
 
 
 def test_set_data():
     data = pd.DataFrame({"Huevos": [1, 2, 3], "Gallinas": [1, 2, 3]})
-    obtained = dt.set_eggs_by_hen(data)
+    obtained = set_eggs_by_hen(data)
     expected_column = "eggs_by_hen"
     assert expected_column in obtained.columns
