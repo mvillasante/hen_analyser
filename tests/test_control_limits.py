b2dc5e576306056776cfa2ccefc50eb84ac699db
Mario VB <mario.villasante@ciencias.unam.mx>
2024-11-30 22:46:22 +0000

    🛑🧪 Fail tests

--- tests/test_control_limits.py
+++ tests/test_control_limits.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 
 def test_set_data():
-    data = pd.DataFrame({"Huevos": [1, 2, 3], "Gallinas": [1, 2, 3]})
+    data = pd.DataFrame({"Huevos": [1, 2, 3], "Gallinas": [1, 2, 1]})
     obtained = set_eggs_by_hen(data)
     expected_column = "eggs_by_hen"
     assert expected_column in obtained.columns
+    assert (obtained.eggs_by_hen == [1, 1, 3]).all()
