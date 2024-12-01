a610f16d89fed0bb286c43270dacd2d9e4f318f1
Mario VB <mario.villasante@ciencias.unam.mx>
2024-12-01 00:17:55 +0000

    ♻️  Refactor

--- huevolizer/control_limits.py
+++ huevolizer/control_limits.py
@@ -2,8 +2,9 @@ from bootstrapping_tools import resample_data
 
 
 def calculate_average_per_sample(data):
-    mean_of_two_values = data.rolling(2, step=1, closed="right").mean()
-    return mean_of_two_values.iloc[1::2, :]
+    sample_size = 2
+    mean_of_two_values = data.rolling(sample_size, closed="right").mean()
+    return mean_of_two_values.iloc[1::sample_size, :]
 
 
 def set_eggs_by_hen(raw_data):
