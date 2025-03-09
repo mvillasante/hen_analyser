d7e8efbd7b6ef960e836da993049c02698be63cb
Mario VB <mario.villasante@ciencias.unam.mx>
2025-03-09 00:52:01 +0000

    🛑🧪 Fail tests

--- tests/test_control_limits.py
+++ tests/test_control_limits.py
@@ -9,11 +9,12 @@ import pandas as pd
 import pytest
 
 
-@pytest.mark.skip("not yet")
 def test_x_r_chart_limits_calculator():
     raw_data = pd.DataFrame({"Huevos": [1, 2, 3], "Gallinas": [1, 2, 1]})
     chart_limits_calculator = X_R_limits_calculator()
     chart_limits_calculator.set_data(raw_data)
+    obtained = chart_limits_calculator.data
+    assert "eggs_by_hen" in obtained.columns
 
 
 def test_set_data():
