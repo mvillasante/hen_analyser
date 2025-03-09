f8ac3d9535cd988992146b7b322248200a75a568
Mario VB <mario.villasante@ciencias.unam.mx>
2025-03-09 02:11:15 +0000

    ♻️  Refactor

--- huevolizer/control_limits.py
+++ huevolizer/control_limits.py
@@ -25,14 +25,16 @@ class X_R_limits_calculator:
 
 def calculate_range_per_sample(data):
     sample_size = 2
-    range_of_sample = abs(data.rolling(sample_size).max() - data.rolling(sample_size).min())
-    return range_of_sample.eggs_by_hen[1::sample_size]
+    range_of_sample = abs(
+        data.eggs_by_hen.rolling(sample_size).max() - data.eggs_by_hen.rolling(sample_size).min()
+    )
+    return range_of_sample[1::sample_size]
 
 
 def calculate_average_per_sample(data):
     sample_size = 2
-    mean_of_two_values = data.rolling(sample_size).mean()
-    return mean_of_two_values.eggs_by_hen[1::sample_size]
+    mean_of_two_values = data.eggs_by_hen.rolling(sample_size).mean()
+    return mean_of_two_values[1::sample_size]
 
 
 def set_eggs_by_hen(raw_data):
