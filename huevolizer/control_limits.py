4a6048a972d3d0e3ff3dc9cfb5842f6d78cf2262
Mario VB <mario.villasante@ciencias.unam.mx>
2025-03-09 01:44:20 +0000

    ✅ Pass tests

--- huevolizer/control_limits.py
+++ huevolizer/control_limits.py
@@ -16,6 +16,7 @@ class X_R_limits_calculator:
             "average": mean_X,
             "one_sigma": mean_X + desviation_X,
             "two_sigma": mean_X + 2 * desviation_X,
+            "three_sigma": mean_X + 3 * desviation_X,
         }
 
 
