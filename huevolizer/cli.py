fa0fd1a90ec1bc4e946863ebc0d6b7c5436c88fc
Mario VB <mario.villasante@ciencias.unam.mx>
2025-03-18 01:21:41 +0000

    ✅ Pass tests

--- huevolizer/cli.py
+++ huevolizer/cli.py
@@ -1,3 +1,4 @@
+from huevolizer.control_limits import X_R_limits_calculator
 import pandas as pd
 
 import typer
@@ -7,7 +8,11 @@ cli = typer.Typer()
 
 @cli.command()
 def write_control_chart_data(data_path: str = typer.Option("Path of daily egg data")):
-    pd.read_csv(data_path).to_csv("salida.csv")
+    raw_data = pd.read_csv(data_path)
+    calculator = X_R_limits_calculator(raw_data)
+    pd.DataFrame({"Fecha": raw_data.Fecha.loc[1:], "X": calculator.x_s}).to_csv(
+        "salida.csv", index=False
+    )
 
 
 @cli.command()
