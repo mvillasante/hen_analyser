cdd00434a630120885dced2d40e1fc56c3887868
Mario VB <mario.villasante@ciencias.unam.mx>
2025-03-18 00:50:58 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,3 +9,12 @@ runner = CliRunner()
 def tests_write_control_chart_data():
     result = runner.invoke(cli, ["write-control-chart", "--help"])
     assert result.exit_code == 0
+
+
+def test_version():
+    result = runner.invoke(
+        cli,
+        ["version"],
+    )
+    expected_version = "0.0.1"
+    assert expected_version in result.stdout
