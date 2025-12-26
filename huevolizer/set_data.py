be36fb77d6d1fcc97e19132b33eac361718709a4
Mario VB <mario.villasante@ciencias.unam.mx>
2025-12-26 23:38:51 +0000

    ✅ Pass tests

--- huevolizer/set_data.py
+++ huevolizer/set_data.py
@@ -5,4 +5,4 @@ from numpy import datetime_as_string
 def fill_missing_dates(data):
     indexed_data = data.set_index("Fecha").copy()
     complete_dates = pd.date_range(indexed_data.index.min(), indexed_data.index.max(), freq="D")
-    return indexed_data.reindex(datetime_as_string(complete_dates, unit="D"))
+    return indexed_data.reindex(datetime_as_string(complete_dates, unit="D")).ffill()
