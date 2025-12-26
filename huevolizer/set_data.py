fbaf3a66c8004206028622188fc12d8b63013e8a
Mario VB <mario.villasante@ciencias.unam.mx>
2025-12-26 23:47:46 +0000

    ✅ Pass tests

--- huevolizer/set_data.py
+++ huevolizer/set_data.py
@@ -6,3 +6,7 @@ def fill_missing_dates(data):
     indexed_data = data.set_index("Fecha").copy()
     complete_dates = pd.date_range(indexed_data.index.min(), indexed_data.index.max(), freq="D")
     return indexed_data.reindex(datetime_as_string(complete_dates, unit="D")).ffill()
+
+
+def join_daily_eggs_with_individual_counts(daily_eggs, individuals_count):
+    return daily_eggs.set_index("Fecha").join(individuals_count.set_index("Fecha"), how="left")
