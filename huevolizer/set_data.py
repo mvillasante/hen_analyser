575d13bbe711c11335ed49731473c175709c932d
Mario VB <mario.villasante@ciencias.unam.mx>
2025-12-26 20:17:05 +0000

    ✅ Pass tests

--- huevolizer/set_data.py
+++ huevolizer/set_data.py
@@ -1,2 +1,2 @@
 def fill_missing_dates(data):
-    return data
+    return data.set_index("Fecha", drop=True)
--- tests/test_set_data.py
+++ tests/test_set_data.py
@@ -13,4 +13,4 @@ def test_fill_missing_date():
         }
     )
     obtained = fill_missing_dates(data)
-    assert obtained.first_valid_index() == obtained.Fecha[0]
+    assert obtained.first_valid_index() == obtained.index[0]
