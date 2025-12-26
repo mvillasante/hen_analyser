b01dd8b940f94d7213f49f44cf38d47d9b1caa7f
Mario VB <mario.villasante@ciencias.unam.mx>
2025-12-26 23:45:40 +0000

    🛑🧪 Fail tests

--- tests/test_set_data.py
+++ tests/test_set_data.py
@@ -1,20 +1,32 @@
-from huevolizer.set_data import fill_missing_dates
+from huevolizer.set_data import fill_missing_dates, join_daily_eggs_with_individual_counts
 
 import pandas as pd
 from numpy import datetime_as_string
 
 
+individual_data = pd.DataFrame(
+    {
+        "Fecha": ["2021-04-30", "2021-05-30"],
+        "Gallinas": [16, 15],
+        "Gallos": [2, 2],
+        "Pollos": [8, 7],
+    }
+)
+
+
 def test_fill_missing_date():
-    data = pd.DataFrame(
-        {
-            "Fecha": ["2021-04-30", "2021-05-30"],
-            "Gallinas": [16, 15],
-            "Gallos": [2, 2],
-            "Pollos": [8, 7],
-        }
-    )
-    obtained = fill_missing_dates(data)
+    obtained = fill_missing_dates(individual_data)
     print(obtained)
     assert obtained.index.values[0] == "2021-04-30"
     assert len(obtained) >= 30
     assert obtained.Gallinas.iloc[1] == 16
+
+
+def test_join_daily_eggs_with_individual_counts():
+    daily_eggs_data = pd.DataFrame(
+        {
+            "Fecha": ["2021-04-30", "2021-04-31"],
+            "Huevos": [8, 7],
+        }
+    )
+    obtained = join_daily_eggs_with_individual_counts(daily_eggs_data, individual_data)
