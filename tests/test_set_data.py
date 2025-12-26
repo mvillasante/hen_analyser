fce283e2d504bd75aa9e86e950775eb0f58512d8
Mario VB <mario.villasante@ciencias.unam.mx>
2025-12-26 20:11:28 +0000

    🛑🧪 Fail tests

---
+++ tests/test_set_data.py
@@ -0,0 +1,15 @@
+from huevolizer.set_data import fill_missing_dates
+
+import pandas as pd
+
+
+def test_fill_missing_date():
+    data = pd.DataFrame(
+        {
+            "Fecha": ["2021-04-30", "2021-05-30"],
+            "Gallinas": [16, 15],
+            "Gallos": [2, 2],
+            "Pollos": [8, 7],
+        }
+    )
+    obtained = fill_missing_dates(data)
