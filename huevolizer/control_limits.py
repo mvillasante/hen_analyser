7e958f4950a1d658b260a3a5648f39411e6bf085
Mario VB <mario.villasante@ciencias.unam.mx>
2025-12-27 00:11:52 +0000

    ♻️  Refactor

--- huevolizer/control_limits.py
+++ huevolizer/control_limits.py
@@ -1,10 +1,56 @@
+from huevolizer.set_data import join_daily_eggs_with_individual_counts
 import pandas as pd
+
+
+class xxX_R_limits_calculator:
+    def __init__(self, raw_data: pd.DataFrame, individual_counts: pd.DataFrame):
+        self.data = self.set_data(raw_data, individual_counts)
+        self.x_s = calculate_average_per_sample(self.data)
+        self.r_s = calculate_range_per_sample(self.data)
+
+    def save_x_r(self, output_path: str):
+        pd.DataFrame({"Fecha": self.data.Fecha.loc[1:], "X": self.x_s, "R": self.r_s}).set_index(
+            "Fecha"
+        ).to_csv(output_path)
+
+    def set_data(self, raw_data: pd.DataFrame, individual_counts):
+        joined_data = join_daily_eggs_with_individual_counts(raw_data, individual_counts)
+        return self.set_eggs_by_hen(joined_data)
+
+    def set_eggs_by_hen(self, raw_data):
+        raw_data["eggs_by_hen"] = raw_data.Huevos / raw_data.Gallinas
+        return raw_data
+
+    def get_X_limits(self):
+        mean_X = self.x_s.mean()
+        desviation_X = self.x_s.std()
+        return {
+            "average": mean_X,
+            "one_sigma": mean_X + desviation_X,
+            "two_sigma": mean_X + 2 * desviation_X,
+            "three_sigma": mean_X + 3 * desviation_X,
+            "minus_one_sigma": mean_X - desviation_X,
+            "minus_two_sigma": mean_X - 2 * desviation_X,
+            "minus_three_sigma": mean_X - 3 * desviation_X,
+        }
+
+    def get_R_limits(self):
+        mean_R = self.r_s.mean()
+        desviation_R = self.r_s.std()
+        return {
+            "average": mean_R,
+            "one_sigma": mean_R + desviation_R,
+            "two_sigma": mean_R + 2 * desviation_R,
+            "three_sigma": mean_R + 3 * desviation_R,
+            "minus_one_sigma": mean_R - desviation_R,
+            "minus_two_sigma": mean_R - 2 * desviation_R,
+            "minus_three_sigma": mean_R - 3 * desviation_R,
+        }
 
 
 class X_R_limits_calculator:
     def __init__(self, raw_data: pd.DataFrame):
         self.data = self.set_data(raw_data)
-        print(self.data.dtypes)
         self.x_s = calculate_average_per_sample(self.data)
         self.r_s = calculate_range_per_sample(self.data)
 
--- tests/test_control_limits.py
+++ tests/test_control_limits.py
@@ -2,6 +2,7 @@ from huevolizer.control_limits import (
     calculate_average_per_sample,
     calculate_range_per_sample,
     X_R_limits_calculator,
+    xxX_R_limits_calculator,
 )
 
 import pandas as pd
@@ -22,13 +23,18 @@ def test_x_r_chart_limits_calculator():
 
     individual_data = pd.DataFrame(
         {
-            "Fecha": ["2021-01-01", "2021-04-04"],
-            "Gallinas": [16, 15],
-            "Gallos": [2, 2],
-            "Pollos": [8, 7],
+            "Fecha": [
+                "2021-01-01",
+                "2021-01-02",
+                "2021-01-03",
+                "2021-01-04",
+            ],
+            "Gallinas": [1, 2, 3, 1],
+            "Gallos": [2, 2, 2, 3],
+            "Pollos": [8, 7, 8, 7],
         }
     )
-    chart_limits_calculator = X_R_limits_calculator(raw_data)
+    chart_limits_calculator = xxX_R_limits_calculator(raw_data, individual_data)
     obtained = chart_limits_calculator.data
     assert "eggs_by_hen" in obtained.columns
     obtained = chart_limits_calculator.get_X_limits()
