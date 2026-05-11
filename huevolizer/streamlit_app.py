2c6c8f4b052d89035ee45993a1c6c632e0fdb6a8
Mario Villasante <mario.villasante@ciencias.unam.mx>
2026-05-10 18:56:26 -0600

    🚀 use production data

--- huevolizer/streamlit_app.py
+++ huevolizer/streamlit_app.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 from huevolizer.cli import write_control_chart_data
 
 st.title("Cartas de Control de producción diaria de huevos")
-cartas_path = "cartas.csv"
-raw_data = write_control_chart_data("tests/data/producción_diaria.csv","tests/data/conteo_individuos.csv",output_path=cartas_path)
-data = pd.read_csv(cartas_path)
+X_R_path = "X_R.csv"
+raw_data = write_control_chart_data("data/producción_diaria.csv","data/conteo_individuos.csv",output_path=X_R_path)
+data = pd.read_csv(X_R_path)
 pyshewhart.XbarR(data["Fecha"], data["X"], sample_size=2)
 st.pyplot(plt.gcf())
