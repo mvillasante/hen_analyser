ea2c6a23cef01f0ce40bffceeb990ce4e5b38e51
Mario Villasante <mario.villasante@ciencias.unam.mx>
2026-06-28 19:22:40 -0600

    🧑🏽‍🎨 ordena plantilla

--- huevolizer/streamlit_app.py
+++ huevolizer/streamlit_app.py
@@ -9,13 +9,14 @@ from huevolizer.app_helpers import (
 # --- Page layout ---
 st.title("Cartas de Control de producción diaria de huevos")
 
-col1, col2 = st.columns(2)
-with col1:
-    st.subheader("Agregar nuevo conteo")
-    render_individual_count_form()
-with col2:
-    st.subheader("Agregar huevos del día")
-    render_egg_count_form()
-
 st.subheader("Carta de Control")
 render_control_chart()
+
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Agregar huevos del día")
+    render_egg_count_form()
+with col2:
+    st.subheader("Agregar nuevo conteo")
+    render_individual_count_form()
+
